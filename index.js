0ce0458ca41db9472887c32d2bf30071b8b1dcbd
zemuuv <samuvarg@gmail.com>
2024-12-03 20:09:03 -0500

    desde 0

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const http = require('http');
-
-const server=http.createServer(function(req,res){
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.end('hola mundo');
-});
-server.listen(5000);
